5464b3cd2db589d137548abbf28f3c434d87d99a
Steve Pieper <pieper@isomics.com>
2024-12-20 11:56:27 -1000

    WIP: expose basic framework

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-const a: string = 'hello world'
-console.log(a)
+import * as dcmjs from "dcmjs"
+
+let l = dcmjs.log;
+
+//const a: string = `hello wide world - ${dcmjs.log.getLevel()}`
+const a: string = `hello wide world - ${l.currentLevel}`
+
+function getLog() {
+    return dcmjs.log.currentLevel;
+}
+
+function apply(organizeOptions) {
+    console.log(organizeOptions);
+}
+
+export { getLog, apply };
 
 export default a
