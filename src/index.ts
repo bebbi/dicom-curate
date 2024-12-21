fb96879497d8ada0bfec765b61ea3072907a9b7b
Steve Pieper <pieper@isomics.com>
2024-12-20 18:22:07 -1000

    WIP: add first pass applying remap pipeline
    Uses file system access api to read dicom files,
    apply modification, and write results to output directory.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,60 @@
 import * as dcmjs from "dcmjs"
 
-let l = dcmjs.log;
 
-//const a: string = `hello wide world - ${dcmjs.log.getLevel()}`
-const a: string = `hello wide world - ${l.currentLevel}`
+const a: string = `hello wide world`;
 
 function getLog() {
     return dcmjs.log.currentLevel;
 }
 
-function apply(organizeOptions) {
-    console.log(organizeOptions);
+async function scanDirectory(dir) {
+  const files = [];
+
+  async function traverse(dir, prefix) {
+    for await (const entry of dir.values()) {
+      if (entry.kind === 'file') {
+        const file = await entry.getFile();
+        files.push({
+          path: prefix,
+          name: entry.name,
+          size: file.size,
+          fileHandle: entry,
+        });
+      } else if (entry.kind === 'directory') {
+        await traverse(entry, prefix + "/" + entry.name);
+      }
+    }
+  }
+
+  await traverse(dir, dir.name);
+  return files;
 }
+
+async function apply(organizeOptions) {
+    dcmjs.log.level = dcmjs.log.levels.ERROR;
+    const fileEntryList = await scanDirectory(organizeOptions.inputDirectory);
+
+    fileEntryList.forEach(async (fileEntry, index) => {
+        const file = await fileEntry.fileHandle.getFile();
+        const fileArrayBuffer = await file.arrayBuffer();
+
+        const dicomData = dcmjs.data.DicomMessage.readFile(fileArrayBuffer);
+        const naturalData = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
+        naturalData["PatientAge"] = "021Y";
+        dicomData.dict = dcmjs.data.DicomMetaDictionary.denaturalizeDataset(naturalData);
+        const modifiedArrayBuffer = dicomData.write();
+
+        const fileName = `file-${index}.dcm`;
+        console.log(organizeOptions.outputDirectory);
+        console.log(fileName);
+        const fileHandle = await organizeOptions.outputDirectory.getFileHandle(fileName, { create: true });
+        const writable = await fileHandle.createWritable();
+        await writable.write(modifiedArrayBuffer);
+        await writable.close();
+    });
+
+}
+
 
 export { getLog, apply };
 
