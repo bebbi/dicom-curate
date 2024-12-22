76b464ab031e19962f31b7afa15bbf56f2505a3e
Steve Pieper <pieper@isomics.com>
2024-12-21 18:52:47 -1000

    WIP: Add additional fields for organize example
    Parse the csv and javascript function arguments to the
    oranize apply method.  Next step is to apply these to the
    sample scenario data.

--- src/index.ts
+++ src/index.ts
@@ -31,9 +31,48 @@ async function scanDirectory(dir) {
 }
 
 async function apply(organizeOptions) {
+    console.log(organizeOptions);
+
+    //
+    // first, get the field mappings from the csv file
+    //
+    // assumes all fields are not repeated across rows
+    const csvFile = await organizeOptions.fieldMapping.getFile();
+    const csvText = await csvFile.text();
+    const rows = csvText.trim().split("\n");
+    const headers = rows.slice(0,1)[0].split(",");
+    const fieldMappings = {
+        headers: headers,
+        rowValues : {},
+        rowIndexByFieldValue : {},
+    };
+    headers.forEach((header) => {
+        fieldMappings.rowIndexByFieldValue[header] = {};
+    });
+
+    rows.slice(1).forEach( (row, rowIndex) => {
+        fieldMappings.rowValues[rowIndex] = row.split(",");
+        fieldMappings.rowValues[rowIndex].forEach( (fieldValue, columnIndex) {
+            fieldMappings.rowIndexByFieldValue[headers[columnIndex]][fieldValue] = rowIndex;
+        });
+    });
+
+    console.log("fieldMappings");
+    console.log(fieldMappings);
+
+    //
+    // second, get the mapping functions
+    //
+    const functionsFile = await organizeOptions.mappingFunctions.getFile();
+    const functionsText = await functionsFile.text();
+
+    console.log(functionsText);
+
+    //
+    // Then, scan through the files from the input directory and save them to the output
+    //
     dcmjs.log.level = dcmjs.log.levels.ERROR;
     const fileEntryList = await scanDirectory(organizeOptions.inputDirectory);
-
     fileEntryList.forEach(async (fileEntry, index) => {
         const file = await fileEntry.fileHandle.getFile();
         const fileArrayBuffer = await file.arrayBuffer();
