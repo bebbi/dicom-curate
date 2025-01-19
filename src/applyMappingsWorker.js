d5d1c63aad73c3dffc0d95b4685384e0d509af92
Steve Pieper <pieper@isomics.com>
2025-01-18 19:24:22 -0500

    Collect all mappings before apply, support hex in mapping functions
    Map results now include full list of replacement operations
    and supports nested tag notation.
    
    Addresses #6

--- src/applyMappingsWorker.js
+++ src/applyMappingsWorker.js
@@ -1,6 +1,7 @@
 import * as dcmjs from "dcmjs";
 import * as mapdefaults from "./mapdefaults.js";
 import * as uuid from "uuid";
+import {get as _get, set as _set, unset as _unset, cloneDeep as _cloneDeep} from "lodash";
 
 // value defined here:
 // https://www.rfc-editor.org/rfc/rfc9562#name-uuid-version-5
@@ -64,36 +65,39 @@ async function createNestedDirectories(topLevelDirectoryHandle, path) {
   return currentDirectoryHandle;
 }
 
-async function applyMappings(fileInfo, mappingOptions) {
+function collectMappings(fileInfo, dicomData, mappingOptions) {
 
-    const mapResults = {};
+    // Returns [naturalData, mapResults]
+    // sourceInstanceUID : original UID for this dicomData
+    // filePath : assembled string of path components
+    // mappings are object of the form:
+    // key:
+    //   "path" : string compatible with lodash get/set
+    // value list elements:
+    //   "originalValue" : value from the dicomData
+    //   "mapOperation" : one of "replace", "delete"
+    //   "mappedValue" : replacement for "replace" operation
+    // "anomalies" : a list of text strings describing any unexpected contents of the data
+    const mapResults = {
+        sourceInstanceUID : undefined,
+        filePath : "",
+        mappings : {},
+        anomalies : [],
+    };
 
     //
-    // First, read the dicom instance data from the file handle
-    //
-    const file = await fileInfo.fileHandle.getFile();
-    const fileArrayBuffer = await file.arrayBuffer();
-    // TODO: capture validation data in object and save as part of results object
-    dcmjs.log.setLevel(dcmjs.log.levels.ERROR);
-    dcmjs.log.getLogger("validation.dcmjs").setLevel(dcmjs.log.levels.SILENT);
-    const dicomData = dcmjs.data.DicomMessage.readFile(fileArrayBuffer);
-
-    //
-    // Then, remove private tags
-    // TODO: add option for `allowlist` of private tags
+    // collect private tags and mark them for delete
+    // TODO: put this in recursive function to find nested private tags
+    // TODO: add option for `allowlist` of private tags taken from 3.15E and TCIA table
     for (let hexTag in dicomData.dict) {
         if (Number(hexTag[3] % 2) == 1) {
-            delete dicomData.dict[hexTag];
+            mapResults.mappings["x"+hexTag] = [String(dicomData.dict[hexTag].Value), "delete", undefined];
         }
     }
 
     // Make make the naturalized data so parser code operates on with tags not hex
     const naturalData = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
     mapResults.sourceInstanceUID = naturalData.SOPInstanceUID;
-
-    //
-    // Then, do the actually header mapping
-    //
 
     // create a parser object to be used in the mappingFunctions
     const parser = {
@@ -109,6 +113,9 @@ async function applyMappings(fileInfo, mappingOptions) {
             return mappingOptions.fieldMappings.rowValues[rowIndex][columnIndex];
         },
         getDicom: (tagName) => {
+            if (tagName in dcmjs.data.DicomMetaDictionary.dictionary) { // if in hex like "(0008,0100)"
+                tagName = dcmjs.data.DicomMetaDictionary.dictionary[tagName].name;
+            }
             return(naturalData[tagName]);
         },
         addDays: (dicomDateString, offsetDays) => {
@@ -134,56 +141,102 @@ async function applyMappings(fileInfo, mappingOptions) {
     mapResults.filePath = filePath.join("/");
 
     // collect the tag mappings before assigning them into dicomData
-    mapResults.tagMappings = {};
-    for (let tag in dicom) {
-        mapResults.tagMappings[tag] = dicom[tag]();
-    }
-    for (let tag in mapResults.tagMappings) {
-        naturalData[tag] = mapResults.tagMappings[tag];
+    // - Note the mappingFunctions return a dictionary called 'dicom' of functions to call
+    //   for each tag they want to map
+    for (let tagPath in dicom) {
+        mapResults.mappings[tagPath] = [_get(naturalData, tagPath), "replace", dicom[tagPath]()];
     }
 
-    // Now apply the standard mappings
+    // Now collect the standard mappings with a
+    // recursive function to handle sequences (naturalData is a top-level instance of data).
+    // Calls modify mapResults in the outer function scope.
     const nameMap = dcmjs.data.DicomMetaDictionary.nameMap;
-    // Recursive function to handle sequences (naturalData is a top-level data)
-    function mapTagsInData (data) {
-        for (let tag of Object.keys(data)) {
+    function collectMappingsInData (data, path="") {
+        for (let tag in data) {
             if (/_.*/.test(tag)) {
                 continue; // ignore tags marked internal with leading underscore
             }
+            const tagPath = path+tag;
             if (tag in nameMap) { // means it's a known tag
                 let vr = nameMap[tag].vr;
                 if (vr == "SQ") {
-                    for (let subdata of Object.values(data[tag])) {
-                        mapTagsInData(subdata);
+                    let subDataIndex = 0;
+                    for (let subData of Object.values(data[tag])) {
+                        let subPath = `${path}${tag}[${subDataIndex}].`;
+                        collectMappingsInData(subData, subPath);
+                        subDataIndex += 1;
                     }
                 } else {
                   if (vr == "UI") {
                       if (mapdefaults.instanceUIDs.indexOf(tag) != -1) { // UIDs that need to be mapped
                           const uid = data[tag];
                           const mappedUID = uidToV5BasedUID(uid);
-                          data[tag] = mappedUID;
-                          mapResults.tagMappings[tag] = mappedUID;
+                          mapResults.mappings[tagPath] = [uid, "replace", mappedUID];
                       }
                   } else {
                       // other tags are handled according to mapdefaults rules
                       if (tag in mapdefaults.tagNamesToEmpty) {
-                          delete data[tag];
+                          mapResults.mappings[tagPath] = [data[tag], "delete", undefined];
                       } else {
                           if (! tag in mapdefaults.tagNamesToAlwaysKeep) {
-                              console.error(`instance contains tag ${tag} that is not defined in mapdefaults.  Deleting it.`);
-                              delete data[tag];
+                              mapResults.anomalies.push(`instance contains tag ${tag} that is not defined in mapdefaults.  Marking it for deletion.`);
+                              mapResults.mappings[tagPath] = [data[tag], "delete", undefined];
                           }
                       }
                   }
                }
             } else {
-                // TODO: this should go in the validation log
-                console.error(`instance contains non-private tag ${tag} that is not in dictionary.  Deleting it.`);
-                delete data[tag];
+                mapResults.anomalies.push(`instance contains non-private tag ${tag} that is not in dictionary.  Marking it for deletion.`);
+                mapResults.mappings[tagPath] = [data[tag], "delete", undefined];
             }
         }
     }
-    mapTagsInData(naturalData);
+    collectMappingsInData(naturalData);
+
+    return [naturalData, mapResults];
+}
+
+async function applyMappings(fileInfo, mappingOptions) {
+
+    //
+    // First, read the dicom instance data from the file handle
+    //
+    const file = await fileInfo.fileHandle.getFile();
+    const fileArrayBuffer = await file.arrayBuffer();
+    // TODO: capture validation data in object and save as part of results object
+    dcmjs.log.setLevel(dcmjs.log.levels.ERROR);
+    dcmjs.log.getLogger("validation.dcmjs").setLevel(dcmjs.log.levels.SILENT);
+    let dicomData;
+    try {
+      dicomData = dcmjs.data.DicomMessage.readFile(fileArrayBuffer);
+    } catch(error) {
+      const mapResults = {
+        anomalies : [`Could not parse ${fileInfo.name} as dicom data`)]
+      };
+      return mapResults;
+    }
+
+    //
+    // then collect the mappings and apply them to the data
+    //
+    const [naturalData, mapResults] = collectMappings(fileInfo, dicomData, mappingOptions);
+    for (let tagPath in mapResults.mappings) {
+        const [_, operation, mappedValue] = mapResults.mappings[tagPath];
+        switch (operation) {
+          case "delete":
+            _unset(naturalData, tagPath);
+          break;
+          case "replace":
+            _set(naturalData, tagPath, mappedValue);
+          break;
+          default:
+            console.error(`Bad operation ${operation} in mappings`);
+        }
+    }
+
+// TODO: apply mappings to metaheader information
+// TODO: doublecheck logic for private tags in sequences (check dictionary version)
+
 
     // Finally, write the results
     const dirPath = mapResults.filePath.split("/").slice(0,-1).join("/");
@@ -200,5 +253,6 @@ async function applyMappings(fileInfo, mappingOptions) {
         await writable.close();
     }
 
-    return mapResults;
+    const clonedMapResults = _cloneDeep(mapResults);
+    return clonedMapResults;
 }
