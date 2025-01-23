41fd54ff831e392a3fab9b1e487427fc29d0fe9b
Stefan <stefan@gigmade.com>
2025-01-23 13:33:45 +0100

    Prettier run across codebase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-
 //const mappingWorkerCount = 2;
-const mappingWorkerCount = navigator.hardwareConcurrency;
-
+const mappingWorkerCount = navigator.hardwareConcurrency
 
 //
 // Directory scanner web worker management
@@ -14,28 +12,29 @@ const mappingWorkerCount = navigator.hardwareConcurrency;
 //   response: 'done'
 //
 // TODO: implement a buffering stream to request fileHandles in batches
-const filesToProcess = [];
-const fileListWorker = new Worker(new URL("./scanDirectoryWorker.js", import.meta.url));
-let directoryScanFinished = false;
-fileListWorker.addEventListener("message", (event) => {
+const filesToProcess = []
+const fileListWorker = new Worker(
+  new URL('./scanDirectoryWorker.js', import.meta.url),
+)
+let directoryScanFinished = false
+fileListWorker.addEventListener('message', (event) => {
   switch (event.data.response) {
     case 'file':
-      filesToProcess.push(event.data.fileInfo);
+      filesToProcess.push(event.data.fileInfo)
       if (filesToProcess.length > 10) {
-        fileListWorker.postMessage({ request: "stop" });
+        fileListWorker.postMessage({ request: 'stop' })
       }
-      dispatchMappingJobs();
-      break;
+      dispatchMappingJobs()
+      break
     case 'done':
-      console.log("directoryScanFinished");
-      directoryScanFinished = true;
-      break;
+      console.log('directoryScanFinished')
+      directoryScanFinished = true
+      break
     default:
-      console.error(`Unknown response from worker ${event.data.response}`);
+      console.error(`Unknown response from worker ${event.data.response}`)
   }
-  dispatchMappingJobs();
-});
-
+  dispatchMappingJobs()
+})
 
 //
 // Apply mappings web worker management
@@ -45,94 +44,104 @@ fileListWorker.addEventListener("message", (event) => {
 // worker sends these messages:
 //   response: 'finished', mapResults
 //
-let mappingOptions = {}; // TODO: only send to worker once
-const availableMappingWorkers = [];
-let workersActive = 0;
-const mappingResultList = [];
+let mappingOptions = {} // TODO: only send to worker once
+const availableMappingWorkers = []
+let workersActive = 0
+const mappingResultList = []
 
 for (let workerIndex = 0; workerIndex < mappingWorkerCount; workerIndex++) {
-  let mappingWorker = new Worker(new URL("./applyMappingsWorker.js", import.meta.url), {type: 'module'});
-  mappingWorker.onerror = console.error;
+  let mappingWorker = new Worker(
+    new URL('./applyMappingsWorker.js', import.meta.url),
+    { type: 'module' },
+  )
+  mappingWorker.onerror = console.error
 
-  mappingWorker.addEventListener("message", (event) => {
+  mappingWorker.addEventListener('message', (event) => {
     switch (event.data.response) {
       case 'finished':
-        availableMappingWorkers.push(mappingWorker);
-        mappingResultList.push(event.data.mapResults);
-        workersActive -= 1;
-        dispatchMappingJobs();
-        if ((mappingResultList.length-1) % 100 == 0) {
-          console.log(`Finished mapping ${mappingResultList.length} files`);
+        availableMappingWorkers.push(mappingWorker)
+        mappingResultList.push(event.data.mapResults)
+        workersActive -= 1
+        dispatchMappingJobs()
+        if ((mappingResultList.length - 1) % 100 == 0) {
+          console.log(`Finished mapping ${mappingResultList.length} files`)
         }
-        break;
+        break
       default:
-        console.error(`Unknown response from worker ${event.data.response}`);
+        console.error(`Unknown response from worker ${event.data.response}`)
     }
-  });
+  })
 
-  availableMappingWorkers.push(mappingWorker);
+  availableMappingWorkers.push(mappingWorker)
 }
 
 function dispatchMappingJobs() {
   while (filesToProcess.length > 0 && availableMappingWorkers.length > 0) {
-    const fileInfo = filesToProcess.pop();
-    const mappingWorker = availableMappingWorkers.pop();
+    const fileInfo = filesToProcess.pop()
+    const mappingWorker = availableMappingWorkers.pop()
     mappingWorker.postMessage({
       request: 'apply',
       fileInfo: fileInfo,
       mappingOptions: mappingOptions,
-    });
-    workersActive += 1;
+    })
+    workersActive += 1
   }
-  if ( workersActive == 0 && directoryScanFinished && filesToProcess.length == 0 ) {
-    console.log("job is finished");
-    console.log(mappingResultList);
+  if (
+    workersActive == 0 &&
+    directoryScanFinished &&
+    filesToProcess.length == 0
+  ) {
+    console.log('job is finished')
+    console.log(mappingResultList)
   }
 }
 
 async function collectMappingOptions(organizeOptions) {
+  //
+  // first, get the folder mappings and set output directory
+  //
+  mappingOptions.folderMappings = organizeOptions.filePathPattern
+  mappingOptions.outputDirectory = organizeOptions.outputDirectory
 
-    //
-    // first, get the folder mappings and set output directory
-    //
-    mappingOptions.folderMappings = organizeOptions.filePathPattern;
-    mappingOptions.outputDirectory = organizeOptions.outputDirectory;
+  //
+  // then, get the field mappings from the csv file
+  //
+  // assumes all fields are not repeated across rows
+  const csvFile = await organizeOptions.fieldMapping.getFile()
+  const csvText = await csvFile.text()
+  const rows = csvText.trim().split('\n')
+  const headers = rows.slice(0, 1)[0].split(',')
+  const fieldMappings = {
+    headers: headers,
+    rowValues: {},
+    rowIndexByFieldValue: {},
+  }
+  headers.forEach((header) => {
+    fieldMappings.rowIndexByFieldValue[header] = {}
+  })
+  rows.slice(1).forEach((row, rowIndex) => {
+    fieldMappings.rowValues[rowIndex] = row.split(',')
+    fieldMappings.rowValues[rowIndex].forEach((fieldValue, columnIndex) => {
+      fieldMappings.rowIndexByFieldValue[headers[columnIndex]][fieldValue] =
+        rowIndex
+    })
+  })
+  mappingOptions.fieldMappings = fieldMappings
 
-    //
-    // then, get the field mappings from the csv file
-    //
-    // assumes all fields are not repeated across rows
-    const csvFile = await organizeOptions.fieldMapping.getFile();
-    const csvText = await csvFile.text();
-    const rows = csvText.trim().split("\n");
-    const headers = rows.slice(0,1)[0].split(",");
-    const fieldMappings = {
-        headers: headers,
-        rowValues : {},
-        rowIndexByFieldValue : {},
-    };
-    headers.forEach((header) => {
-        fieldMappings.rowIndexByFieldValue[header] = {};
-    });
-    rows.slice(1).forEach( (row, rowIndex) => {
-        fieldMappings.rowValues[rowIndex] = row.split(",");
-        fieldMappings.rowValues[rowIndex].forEach( (fieldValue, columnIndex) => {
-            fieldMappings.rowIndexByFieldValue[headers[columnIndex]][fieldValue] = rowIndex;
-        });
-    });
-    mappingOptions.fieldMappings = fieldMappings;
-
-    //
-    // then, get the mapping functions
-    //
-    const functionsFile = await organizeOptions.mappingFunctions.getFile();
-    mappingOptions.mappingFunctions = await functionsFile.text();
+  //
+  // then, get the mapping functions
+  //
+  const functionsFile = await organizeOptions.mappingFunctions.getFile()
+  mappingOptions.mappingFunctions = await functionsFile.text()
 }
 
 async function apply(organizeOptions) {
-  fileListWorker.postMessage({ request: "scan", directoryHandle: organizeOptions.inputDirectory });
-  await collectMappingOptions(organizeOptions); // sets global mappingOptions
-  dispatchMappingJobs();
+  fileListWorker.postMessage({
+    request: 'scan',
+    directoryHandle: organizeOptions.inputDirectory,
+  })
+  await collectMappingOptions(organizeOptions) // sets global mappingOptions
+  dispatchMappingJobs()
 }
 
-export { apply };
+export { apply }
--- src/scanDirectoryWorker.js
+++ src/scanDirectoryWorker.js
@@ -1,46 +1,45 @@
+let keepScanning = true
 
-let keepScanning = true;
-
-self.addEventListener("message", (event) => {
+self.addEventListener('message', (event) => {
   switch (event.data.request) {
     case 'scan':
-      console.log(`Starting directory scan of ${event.data.directoryHandle.name}`);
-      keepScanning = true;
-      scanDirectory(event.data.directoryHandle);
-      break;
+      console.log(
+        `Starting directory scan of ${event.data.directoryHandle.name}`,
+      )
+      keepScanning = true
+      scanDirectory(event.data.directoryHandle)
+      break
     case 'stop':
-      keepScanning = false;
-      break;
+      keepScanning = false
+      break
     default:
-      console.error(`Unknown request ${event.data.request}`);
+      console.error(`Unknown request ${event.data.request}`)
   }
-});
-
+})
 
 async function scanDirectory(dir) {
-
   async function traverse(dir, prefix) {
     for await (const entry of dir.values()) {
       if (entry.kind === 'file') {
-        const file = await entry.getFile();
+        const file = await entry.getFile()
         self.postMessage({
-          response: "file",
+          response: 'file',
           fileInfo: {
             path: prefix,
             name: entry.name,
             size: file.size,
             fileHandle: entry,
-          }
-        });
+          },
+        })
       } else if (entry.kind === 'directory') {
         if (keepScanning) {
-          await traverse(entry, prefix + "/" + entry.name);
+          await traverse(entry, prefix + '/' + entry.name)
         }
       }
     }
   }
 
-  await traverse(dir, dir.name);
-  self.postMessage({ response: "done" });
-  self.close();
+  await traverse(dir, dir.name)
+  self.postMessage({ response: 'done' })
+  self.close()
 }
