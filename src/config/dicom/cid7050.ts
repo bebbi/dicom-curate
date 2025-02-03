20c309d9ff5e247ab075e78842c2e744ecca6251
Stefan <stefan@gigmade.com>
2025-02-03 09:57:12 +0100

    Add correct CID 7050 codelists to the anonymization procedure to be documented in the DICOM header.
    - For each selected option, add a CID 7050 { code, meaning, scheme } triplet
    - Extend the types for possible replacement values with sequences

---
+++ src/config/dicom/cid7050.ts
@@ -0,0 +1,55 @@
+import type { TPS315Options } from '../../types'
+
+export const supportedCid7050: Record<
+  | keyof Omit<TPS315Options, 'retainLongitudinalTemporalInformationOptions'>
+  | 'retainLongitudinalTemporalInformationFullDatesOptions'
+  | 'retainLongitudinalTemporalInformationModifiedDatesOptions'
+  | 'basicApplicationConfidentialityProfile',
+  { value: string; meaning: string; scheme: 'DCM' }
+> = {
+  basicApplicationConfidentialityProfile: {
+    value: '113100',
+    meaning: 'Basic Application Confidentiality Profile',
+    scheme: 'DCM',
+  },
+  cleanDescriptorsOption: {
+    value: '113105',
+    meaning: 'Clean Descriptors Option',
+    scheme: 'DCM',
+  },
+  retainLongitudinalTemporalInformationFullDatesOptions: {
+    value: '113106',
+    meaning: 'Retain Longitudinal Temporal Information Full Dates Option',
+    scheme: 'DCM',
+  },
+  retainLongitudinalTemporalInformationModifiedDatesOptions: {
+    value: '113107',
+    meaning: 'Retain Longitudinal Temporal Information Modified Dates Option',
+    scheme: 'DCM',
+  },
+  retainPatientCharacteristicsOption: {
+    value: '113108',
+    meaning: 'Retain Patient Characteristics Option',
+    scheme: 'DCM',
+  },
+  retainDeviceIdentityOption: {
+    value: '113109',
+    meaning: 'Retain Device Identity Option',
+    scheme: 'DCM',
+  },
+  retainUIDsOption: {
+    value: '113110',
+    meaning: 'Retain UIDs Option',
+    scheme: 'DCM',
+  },
+  retainSafePrivateOption: {
+    value: '113111',
+    meaning: 'Retain Safe Private Option',
+    scheme: 'DCM',
+  },
+  retainInstitutionIdentityOption: {
+    value: '113112',
+    meaning: 'Retain Institution Identity Option',
+    scheme: 'DCM',
+  },
+}
--- src/config/dicom/dcmOrganizeStamp.ts
+++ src/config/dicom/dcmOrganizeStamp.ts
@@ -1,4 +1,41 @@
-export const dcmOrganizeStamp = {
-  PatientIdentityRemoved: 'YES',
-  DeidentificationMethod: 'dcm-organize',
+import { supportedCid7050 } from './cid7050'
+import type { TPS315Options } from '../../types'
+
+export function getDcmOrganizeStamp(options: TPS315Options) {
+  return {
+    PatientIdentityRemoved: 'YES',
+    DeidentificationMethod: 'See dcm-organize README for details',
+    DeidentificationMethodCodeSequence: getCid7050Codes(options).map(
+      (option) => ({
+        CodeValue: option.value,
+        CodeMeaning: option.meaning,
+        CodingSchemeDesignator: option.scheme,
+      }),
+    ),
+  }
+}
+
+export function getCid7050Codes(options: TPS315Options) {
+  const seq = [supportedCid7050.basicApplicationConfidentialityProfile]
+
+  Object.entries(options).forEach(([_option, v]) => {
+    const option = _option as keyof TPS315Options
+
+    if (v === false || v === 'Off') {
+      return
+    }
+
+    const item =
+      option === 'retainLongitudinalTemporalInformationOptions'
+        ? v === 'Full'
+          ? supportedCid7050.retainLongitudinalTemporalInformationFullDatesOptions
+          : supportedCid7050.retainLongitudinalTemporalInformationModifiedDatesOptions
+        : supportedCid7050[option]
+
+    seq.push(item)
+  })
+
+  seq.sort((a, b) => Number(a.value) - Number(b.value))
+
+  return seq
 }
