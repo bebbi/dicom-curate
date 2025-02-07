e3fbb1180906726bb5ad251678ab579b7b75419d
Stefan <stefan@gigmade.com>
2025-02-07 18:37:32 +0100

    If uid retain option is active, do not map metaheader

--- src/mapMetaheader.ts
+++ src/mapMetaheader.ts
@@ -1,15 +1,16 @@
 import * as dcmjs from 'dcmjs'
-import uidToV5BasedUID from './uidToV5BasedUID'
 import type { TDicomDataDict } from 'dcmjs'
 import { metaheaderTagsToKeep } from './config/dicom/metaheaderTagsToKeep'
 
 const EXPLICIT_LITTLE_ENDIAN = '1.2.840.10008.1.2.1'
 
-export default function mapMetaheader(metaHeader: TDicomDataDict) {
+export default function mapMetaheader(
+  metaHeader: TDicomDataDict,
+  // mapped UID or original depending on activated PS3.15E option
+  newInstanceUid: string,
+) {
   const naturalMetadata =
     dcmjs.data.DicomMetaDictionary.naturalizeDataset(metaHeader)
-  // save the UID to add back later
-  const instanceUID = naturalMetadata.MediaStorageSOPInstanceUID
   // keep only the bare set of tags needed to make valid metaheader
   for (let tag in naturalMetadata) {
     if (metaheaderTagsToKeep.indexOf(tag) === -1) {
@@ -17,8 +18,7 @@ export default function mapMetaheader(metaHeader: TDicomDataDict) {
     }
   }
   // add the UID and transfer syntax explicitly
-  const mappedUID = uidToV5BasedUID(instanceUID)
-  naturalMetadata.MediaStorageSOPInstanceUID = mappedUID
+  naturalMetadata.MediaStorageSOPInstanceUID = newInstanceUid
   naturalMetadata.TransferSynxtaxUID = EXPLICIT_LITTLE_ENDIAN // dcmjs always writes this
   const mappedMetaheader =
     dcmjs.data.DicomMetaDictionary.denaturalizeDataset(naturalMetadata)
