5272e5e2d8615caa00ced0513e532a30df5bc03a
Stefan Baumann <elghatta@gmail.com>
2025-05-29 10:43:00 +0200

    Remove the import from the curation spec as that does not work. Make a more targeted sample spec for the 2-pass scenario.

--- src/config/sample2PassCurationSpecification.ts
+++ src/config/sample2PassCurationSpecification.ts
@@ -1,4 +1,3 @@
-import { sampleBatchCurationSpecification } from './sampleBatchCurationSpecification'
 import type { TCurationSpecification } from '../types'
 
 /*
@@ -7,10 +6,20 @@ import type { TCurationSpecification } from '../types'
  * Second pass: load the mapping output from the CSV file
  */
 export function sample2PassCurationSpecification(): TCurationSpecification {
-  const batchSpec = sampleBatchCurationSpecification()
-  const identifiers = batchSpec.identifiers
+  // Confirm allowed identifiers for this transfer.
+  const identifiers = {
+    protocolNumber: 'Sample_Protocol_Number',
+    activityProviderName: 'Sample_CRO',
+    centerSubjectId: /^[A-Z]{2}\d{2}-\d{3}$/,
+    timepointNames: ['Visit 1', 'Visit 2', 'Visit 3', 'EXCLUDE'],
+    scanNames: ['PET-CT', 'PET', 'CT'],
+  }
+
   return {
-    ...batchSpec,
+    // Review the required input folder structure (all DICOM files need minimally this folder depth)
+    // This configuration depends on correct centerSubjectId, timepoint, scan folder names.
+    inputPathPattern: 'any',
+
     additionalData: {
       // type listing is to collect the mapping input in a first pass.
       type: 'listing',
@@ -20,7 +29,7 @@ export function sample2PassCurationSpecification(): TCurationSpecification {
           PatNamePatId: ['PatientName', 'PatientID']
             .map(parser.getDicom)
             .join('='),
-          PatNameSeriesDesc: ['PatientName', 'PatientID', 'SeriesDescription']
+          PatNameIDSeriesDesc: ['PatientName', 'PatientID', 'SeriesDescription']
             .map(parser.getDicom)
             .join('='),
         }
@@ -28,6 +37,7 @@ export function sample2PassCurationSpecification(): TCurationSpecification {
           lookups,
           info: [
             // [label, value]
+            ['Protocol Number', identifiers.protocolNumber],
             ['Patient Name', parser.getDicom('PatientName')],
             ['Patient ID', parser.getDicom('PatientID')],
             ['Study Date', parser.getDicom('StudyDate')],
@@ -37,38 +47,143 @@ export function sample2PassCurationSpecification(): TCurationSpecification {
             ['Modality', parser.getDicom('Modality')],
           ],
           // This will imply a CSV with headers:
-          // PatNamePatId | PatNameSeriesDesc | CenterSubjectId | Timepoint | Comment
+          // PatNamePatId | PatNameIDSeriesDesc | CenterSubjectId | Timepoint | Comment
           // [lookup header, format, lookup value]
           collect: [
             ['CenterSubjectId', identifiers.centerSubjectId, 'PatNamePatId'],
-            ['Timepoint', identifiers.timepointNames, 'PatNameSeriesDesc'],
-            ['Comment', /.*/, 'PatNameSeriesDesc'],
+            // For now, collecting this on Series not Study level.
+            // Trade-off is re-use (study level) vs avoiding confusion due to
+            // any mismatch on study vs series level (e.g. 2 studies one visit)
+            ['Timepoint', identifiers.timepointNames, 'PatNameIDSeriesDesc'],
+            ['ScanName', identifiers.scanNames, 'PatNameIDSeriesDesc'],
+            ['Comment', /.*/, 'PatNameIDSeriesDesc'],
           ],
         }
       },
-      // type: 'load',
-      // collect: {
-      //   CURR_ID: identifiers.centerSubjectId,
-      //   StudyDescription: identifiers.timepointNames,
-      //   MAPPED_ID: /BLIND_\d+/,
-      // },
       // With this, can refer to mappings as parser.getMapping('blindedId')
       mapping: {
-        // csv situation
-        blindedId: {
-          value: (parser) => parser.getDicom('PatientID'),
-          lookup: (row) => row['CURR_ID'],
-          replace: (row) => row['MAPPED_ID'],
-        },
         // firstPass situation
         centerSubjectId: {
           value: (parser) =>
             ['PatientName', 'PatientID'].map(parser.getDicom).join('='),
-          // TODO: could rely on lookup and not need the combined field or field name
           lookup: (row) => row['PatNamePatId'],
           replace: (row) => row['CenterSubjectId'],
         },
+        timepoint: {
+          value: (parser) =>
+            ['PatientName', 'PatientID', 'SeriesDescription']
+              .map(parser.getDicom)
+              .join('='),
+          lookup: (row) => row['PatNameIDSeriesDesc'],
+          replace: (row) => row['Timepoint'],
+        },
+        scanName: {
+          value: (parser) =>
+            ['PatientName', 'PatientID', 'SeriesDescription']
+              .map(parser.getDicom)
+              .join('='),
+          lookup: (row) => row['PatNameIDSeriesDesc'],
+          replace: (row) => row['ScanName'],
+        },
       },
+    },
+
+    version: '1.1',
+    identifiers,
+
+    // This specifies the standardized DICOM de-identification
+    dicomPS315EOptions: {
+      cleanDescriptorsOption: true,
+      cleanDescriptorsExceptions: ['SeriesDescription'],
+      retainLongitudinalTemporalInformationOptions: 'Full',
+      retainPatientCharacteristicsOption: [
+        'PatientsWeight',
+        'PatientsSize',
+        'PatientsAge',
+        'PatientsSex',
+        'SelectorASValue',
+      ],
+      retainDeviceIdentityOption: true,
+      retainUIDsOption: 'Hashed',
+      retainSafePrivateOption: 'Quarantine',
+      retainInstitutionIdentityOption: true,
+    },
+
+    // This section defines the output folder structure and alignment of DICOM headers
+    modifications(parser) {
+      const centerSubjectId = String(parser.getMapping!('centerSubjectId'))
+      const timepoint = String(parser.getMapping!('timepoint'))
+      const scanName = String(parser.getMapping!('scanName'))
+
+      return {
+        dicomHeader: {
+          // Align the PatientID DICOM header with the centerSubjectId folder name.
+          PatientID: centerSubjectId,
+          // This example maps PatientIDs based on the mapping CSV file.
+          // PatientID: parser.getMapping('blindedId'),
+          PatientName: centerSubjectId,
+          // Align the StudyDescription DICOM header with the timepoint folder name.
+          StudyDescription: timepoint,
+          // The party responsible for assigning a standard ClinicalTrialSeriesDescription
+          // ClinicalTrialCoordinatingCenterName: identifiers.activityProviderName,
+          // Align the ClinicalTrialSeriesDescription DICOM header with the scan folder name.
+          ClinicalTrialSeriesDescription: scanName,
+        },
+
+        // This defines the output folder structure.
+        outputFilePathComponents: [
+          identifiers.protocolNumber,
+          centerSubjectId,
+          timepoint,
+          scanName + '=' + parser.getDicom('SeriesNumber'),
+          parser.getFilePathComp(parser.FILEBASENAME) + '.dcm',
+        ],
+      }
+    },
+
+    // This section defines the validation rules for the input DICOMs.
+    // The processing continues on errors, but errors will have to be fixed
+    // or reviewed between the parties.
+    validation(parser) {
+      const filename = parser.getFilePathComp(parser.FILENAME)
+      const seriesUid = parser.getDicom('SeriesInstanceUID')
+      const centerSubjectId = String(parser.getMapping!('centerSubjectId'))
+      const timepoint = String(parser.getMapping!('timepoint'))
+      const scanName = String(parser.getMapping!('scanName'))
+
+      return {
+        errors: [
+          // File path
+          [
+            'Invalid site-subject format',
+            !centerSubjectId.match(identifiers.centerSubjectId),
+          ],
+          [
+            'Invalid timepoint descriptor',
+            !identifiers.timepointNames.includes(timepoint),
+          ],
+          [
+            'Invalid scan descriptor',
+            !identifiers.scanNames.includes(scanName),
+          ],
+          // DICOM header
+          ['Missing Modality', parser.missingDicom('Modality')],
+          ['Missing SOP Class UID', parser.missingDicom('SOPClassUID')],
+          [
+            'Duplicate File Name(s) in series',
+            !parser.isUniqueInGroup(filename, seriesUid),
+          ],
+          [
+            'Missing Series Instance UID',
+            parser.missingDicom('SeriesInstanceUID'),
+          ],
+          [
+            'Missing Study Instance UID',
+            parser.missingDicom('StudyInstanceUID'),
+          ],
+          ['Missing SOP Instance UID', parser.missingDicom('SOPInstanceUID')],
+        ],
+      }
     },
   }
 }
