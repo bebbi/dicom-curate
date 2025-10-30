0a9993b9b2d0f1924f94d5ab1a03dd2e68c218e1
Lubos Dolezel <lubos@dolezel.info>
2025-10-30 22:27:45 +0100

    feat: ensure patient birth/death date in alt calendar is also protected

---
+++ scripts/data/additionalElementsToProtect.js
@@ -0,0 +1,5 @@
+// Known sensitive tags that are missing from PS3.15E1.1
+export const additionalElementsToProtect = [
+  '(0010,0033)', // Patient's Birth Date in Alternative Calendar
+  '(0010,0034)', // Patient's Death Date in Alternative Calendar
+]
