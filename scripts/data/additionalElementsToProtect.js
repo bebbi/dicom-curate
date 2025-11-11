187c1505c3d3a63a9c3893e6d7bf935cdee32b16
Stefan <elghatta@gmail.com>
2025-11-11 14:03:45 +0100

    feat: manual review and extension of attributes to protect that are not in PS3.15E1.1

--- scripts/data/additionalElementsToProtect.js
+++ scripts/data/additionalElementsToProtect.js
@@ -1,5 +1,29 @@
 // Known sensitive tags that are missing from PS3.15E1.1
 export const additionalElementsToProtect = [
+  // XxDescriptionTrial = 'Xx Description/Comment (Trial)': more descriptions, retired
+  'AlgorithmDescriptionTrial',
+  'AnatomicLocationOfExaminingInstrumentDescriptionTrial',
+  'AnatomicPerspectiveDescriptionTrial',
+  'IdentificationDescriptionTrial',
+  'MeasurementPrecisionDescriptionTrial',
+  'ReportStatusCommentTrial',
+  'RequestedProcedureDescriptionTrial',
+  'SpecimenDescriptionTrial',
+  // 'CommentsOn..': alternative comments naming convention
+  'CommentsOnPatientPerformanceOfVisualField',
+  'CommentsOnRadiationDose',
+  'CommentsOnThePerformedProcedureStep',
+  'CommentsOnTheScheduledProcedureStep',
+  // General possibly identifiable attributes
+  'ContactDisplayName',
+  'ContactMethod',
+  'ContactURI',
+  'InstructionText',
+  'LabelText',
+  'PatientAlternativeCalendar',
+  'PatientBirthDateInAlternativeCalendar',
   'PatientBirthDateInAlternativeCalendar',
   'PatientDeathDateInAlternativeCalendar',
+  'PatientDeathDateInAlternativeCalendar',
+  'SecondaryReviewerName',
 ]
