1ca71d622488764ab3627d0dccccc248d6648272
Zak Catherall <zakcatherall@Admins-MacBook-Pro.local>
2025-12-04 13:48:41 +0000

    chore: surpress dcmjs errors and warnings in tests

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,25 @@ import { TextEncoder, TextDecoder } from 'util'
 
 global.TextEncoder = global.TextEncoder || TextEncoder
 global.TextDecoder = global.TextDecoder || TextDecoder
+
+// Filter out known dcmjs console errors
+const originalConsoleError = console.error
+const originalConsoleWarn = console.warn
+
+console.error = (...args) => {
+  const message = args[0]?.toString() || ''
+  // Suppress dcmjs VR type warnings
+  if (message.includes('Invalid vr type') || message.includes('using UN')) {
+    return
+  }
+  originalConsoleError.apply(console, args)
+}
+
+console.warn = (...args) => {
+  const message = args[0]?.toString() || ''
+  // Suppress dcmjs unknown name warnings
+  if (message.includes('Unknown name in dataset')) {
+    return
+  }
+  originalConsoleWarn.apply(console, args)
+}
